f189e5b099451c21013ca0d253ea5738283e99ff
beginman <xinxinyu2011@163.com>
2017-12-21 19:12:24 +0800

    fixbug

--- ydao.rb
+++ ydao.rb
@@ -5,8 +5,8 @@
 class Ydao < Formula
   desc "command line youdao"
   homepage "https://github.com/BeginMan/ydao"
-  url "https://github.com/BeginMan/ydao/archive/v1.1.tar.gz"
-  sha256 "430c3ea65fdb4556aec65179a29a5036d0061c59bbbd9c38c21315d872274e69"
+  url "http://beginman.qiniudn.com/ydao-mac64-1.1.tar.gz"
+  sha256 "110a292052c0dbaae78c72942c13829f7260fb8ec5d62fde28eace8598b56a5a"
 
   # depends_on "cmake" => :build
   depends_on "mpg123" => :run
@@ -16,15 +16,6 @@ class Ydao < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ydao`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/ydao", "python"
   end
 end
