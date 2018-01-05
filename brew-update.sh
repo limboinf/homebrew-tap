d22428b1e371f888167e00ce6ff70cfc3146c35e
beginman <xinxinyu2011@163.com>
2018-01-05 16:29:01 +0800

    update ydao brew version

---
+++ brew-update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ $# != 1 ];then
+    echo "Usage: ./brew-update.sh <version>"
+    exit 0
+fi
+
+VERSION=$1
+PACKAGE="ydao-mac64-${1}.tar.gz"
+URL="http://beginman.qiniudn.com/$PACKAGE"
+
+wget "${URL}"
+sha256="$(openssl dgst -sha256 $PACKAGE | tr '=' '\n')"
+for l in $sha256
+do
+    sha256Value=$l
+done
+
+cat <<EOF > xx.rb
+class Ydao < Formula
+  desc "command line youdao"
+  homepage "https://github.com/BeginMan/ydao"
+  version "$VERSION"
+  url "http://beginman.qiniudn.com/ydao-mac64-$VERSION.tar.gz"
+  sha256 "$sha256Value"
+
+  # depends_on "cmake" => :build
+  depends_on "mpg123" => :run
+
+  def install
+    bin.install "ydao"
+  end
+
+  test do
+    system "#{bin}/ydao", "python"
+  end
+end
+EOF
