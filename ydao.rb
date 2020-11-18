8bfe4802e93f54e5c560cfbb77a51be2d19dc90d
beginman <pythonsuper@gmail.com>
2020-11-18 16:35:07 +0800

    Update ydao.rb

--- ydao.rb
+++ ydao.rb
@@ -6,7 +6,7 @@ class Ydao < Formula
   desc "command line youdao"
   homepage "https://github.com/BeginMan/ydao"
   version "1.2"
-  url "http://beginman.qiniudn.com/ydao-mac64-1.2.tar.gz"
+  url "https://github.com/BeginMan/ydao/releases/download/v1.2.1/ydao-mac64-1.2.tar.gz"
   sha256 "435fff25d9e1c4129cf7d73bebe8d8af69c5d737f190015aa0e1c5213e08d9f0"
 
   # depends_on "cmake" => :build
